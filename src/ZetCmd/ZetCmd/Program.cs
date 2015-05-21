f70157dab4c7de35438f30f9d8f590da3a6398d5
Patrik Lindstrom <patriklindstrom@hotmail.com>
2015-05-21 20:18:20 +0200

    First Baseline with Hello world

---
+++ src/ZetCmd/ZetCmd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZetCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Welcome to ZetCmd a program to perform setbased operation on datafiles.");
+            Console.ReadKey();
+
+        }
+    }
+}

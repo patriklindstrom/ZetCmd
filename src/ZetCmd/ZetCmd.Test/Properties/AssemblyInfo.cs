d563372eaf5327f69bea3be09aed8dc547b1520e
Patrik Lindstrom <patriklindstrom@hotmail.com>
2015-05-23 23:32:22 +0200

    Adding Test
    And hence have to add more framework .

---
+++ src/ZetCmd/ZetCmd.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ZetCmd.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZetCmd.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0ac8f630-c74d-495c-9797-9ceab54b27aa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/ZetCmd/ZetCmd.Test/TestValues.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZetCmd.Test
+{
+    /// <summary>
+    /// Handy MockValues to use wherever in tests.
+    /// </summary>
+    public static class Mv
+    {
+        public static string Line = "fum;fan;foo;bar;king;barter;28;496;8128;;;endisnear;really?";
+        public static string Line2 = "fum;fan;fee;bar;king;stop;28;496;8128;;True;endisnear;really?";
+        public static string Line3 = "456;fan;faa;bar;king;barter;28;496;8128;fist;;endi!snear;rally";
+        public static string Line4 = "NewFi;foo;fum;bar;king;barter;28;496;8128;fist;;endi!snear;rally";
+        public static string cSsepLine = "fum,fan,foo,bar,king,barter,28,49;6,8128,,,endisnear,Wally";
+        public static string cSsepLine2 = "fum,fan,fee,bar,king,stop,28,496,8128,,True,endisnear,really?";
+        public static string cSsepLine3 = "fum,fan,fee,bar,king,stop,28,496,25964951,,False,endisnear,Sally?";
+        public static string ExpectedKey = "foo|bar|barter";
+
+        public static int[] ColKeys = { 3, 4, 6 };
+        public static char Splitchar = ';';
+        public static string DataPath = "C:\\temp\\testpath";
+        public static string Name = "TestName_Tore";
+
+        public static string FileA = "D:\\DataImport\\TestFileA.csv}";
+        public static string FileB = "D:\\DataImport\\TestFileB.csv}";
+        public static bool IntersectAandB = true;
+        public static bool Verbose;
+        public static bool DiffB = true;
+    }
+}

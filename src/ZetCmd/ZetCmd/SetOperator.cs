4eaafeb57b503e1358528f5877c6a0698bd19c82
Patrik Lindstrom <patriklindstrom@hotmail.com>
2015-05-25 23:44:10 +0200

    Lamdba function for SetOperator
    It is now a member of the object

--- src/ZetCmd/ZetCmd/SetOperator.cs
+++ src/ZetCmd/ZetCmd/SetOperator.cs
@@ -16,53 +16,44 @@ namespace ZetCmd
         public DictCompareOnKeyOnly KeyOnly { get; set; }
         public Options Opt { get; set; }
 
+        protected Func
+                <Dictionary<string, string>, Dictionary<string, string>, DictCompareOnKeyOnly,
+                    Dictionary<string, string>> SetOperatorFunc;
         protected SetOperator(DataChunk b,DataChunk a,DictCompareOnKeyOnly keyOnly,Options options)
         {
             BChunk = b;
             AChunk = a;
             KeyOnly = keyOnly;
             Opt = options;
-
         }
 
 
-        // Operate should take lambda function as param.
         public virtual Dictionary<string, string> Operate()
-        {
-
-
-
+        { 
+            var setDiffSw = Stopwatch.StartNew();
+            ReturnDictionary = SetOperatorFunc(AChunk.LineDictionary, BChunk.LineDictionary, KeyOnly);
+            if (Opt.Verbose)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                // TODO: This text about do not exist in file is only true for subclass DiffB. Must move it out from here.
+                Console.WriteLine("File {0} ({4}) has {1} keys. {2} of them do not exist in file {3} ({5})", Path.GetFileName(BChunk.DataPath), BChunk.LineDictionary.Count(), ReturnDictionary.Count(), Path.GetFileName(AChunk.DataPath), BChunk.Name, AChunk.Name);
+                Console.WriteLine("Time after creating set operator {0} ms", setDiffSw.ElapsedMilliseconds);
+            }
+            setDiffSw.Stop();
             return ReturnDictionary;
         }
     }
 
     public class DiffB : SetOperator
-    {
-         
+    {       
         public DiffB(DataChunk b, DataChunk a, DictCompareOnKeyOnly keyOnly, Options options) : base(b, a, keyOnly, options)
         {
             BChunk = b;
             AChunk = a;
             KeyOnly = keyOnly;
             Opt = options;
-        }
-
-
-        public override Dictionary<string, string> Operate()
-        {            // ReturnDictionary = BChunk.LineDictionary.Except(AChunk.LineDictionary, KeyOnly).ToDictionary(ld => ld.Key, ld => ld.Value);
             //Here comes the magic simple Except and Intersect and force it back to Dictionary.
-            var setDiffSw = Stopwatch.StartNew();
-            Func<Dictionary<string, string>, Dictionary<string, string>, DictCompareOnKeyOnly, Dictionary<string, string>> setop = (aDict, bDict, k) => bDict.Except(aDict, k).ToDictionary(ld => ld.Key, ld => ld.Value);
-            ReturnDictionary = setop(AChunk.LineDictionary, BChunk.LineDictionary, KeyOnly);
-             if (Opt.Verbose)
-             {
-                 Console.ForegroundColor = ConsoleColor.Red;
-                 Console.WriteLine("File {0} ({4}) has {1} keys. {2} of them do not exist in file {3} ({5})", Path.GetFileName(BChunk.DataPath), BChunk.LineDictionary.Count(), ReturnDictionary.Count(), Path.GetFileName(AChunk.DataPath), BChunk.Name, AChunk.Name);
-                 Console.WriteLine("Time after creating set operator {0} ms", setDiffSw.ElapsedMilliseconds);
-             }
-             setDiffSw.Stop();
-            return ReturnDictionary;
+            SetOperatorFunc =  (aDict, bDict, k) => bDict.Except(aDict, k).ToDictionary(ld => ld.Key, ld => ld.Value);
         }
-
     }
 }

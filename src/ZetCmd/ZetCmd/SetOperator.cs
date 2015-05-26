e0880a1143d6995737437f68032dbdefdf7634bb
Patrik Lindström <patrik.lindstrom@lcube.se>
2015-05-26 12:53:19 +0200

    Add Union Class
    Added Union Class and cleaned up code

--- src/ZetCmd/ZetCmd/SetOperator.cs
+++ src/ZetCmd/ZetCmd/SetOperator.cs
@@ -9,14 +9,13 @@ namespace ZetCmd
 {
     public abstract class SetOperator
     {
+        #region Properties for SetOperator class
         private Dictionary<string, string> _returnDictionary;
         public Dictionary<string, string> ReturnDictionary
         {
             get
-            {
-                
+            {                
                var setDiffSw = Stopwatch.StartNew();
-
                 try
                 {
                     _returnDictionary = SetOperatorFunc(AChunk.LineDictionary, BChunk.LineDictionary, KeyOnly);
@@ -33,20 +32,19 @@ namespace ZetCmd
                     Console.WriteLine("File {0} ({4}) has {1} keys. {2} of them do not exist in file {3} ({5})", Path.GetFileName(BChunk.DataPath), BChunk.LineDictionary.Count(), _returnDictionary.Count(), Path.GetFileName(AChunk.DataPath), BChunk.Name, AChunk.Name);
                     Console.WriteLine("Time after creating set operator {0} ms", setDiffSw.ElapsedMilliseconds);
                 }
-                setDiffSw.Stop();
-                
+                setDiffSw.Stop();                
                 return _returnDictionary;
             }
         }
-
         public DataChunk AChunk { get; set; }
         public DataChunk BChunk { get; set; }
         public DictCompareOnKeyOnly KeyOnly { get; set; }
         public Options Opt { get; set; }
+        //Here is the clever method that is as disgusied as a function that is set in the subclass constructor method.
         protected Func
                 <Dictionary<string, string>, Dictionary<string, string>, DictCompareOnKeyOnly,
                     Dictionary<string, string>> SetOperatorFunc { get; set; }
-
+        #endregion
         protected SetOperator(DataChunk b,DataChunk a,DictCompareOnKeyOnly keyOnly,Options options)
         {
             BChunk = b;
@@ -64,6 +62,16 @@ namespace ZetCmd
             // Here comes the magic simple Except and Intersect and force it back to Dictionary
             // all in Lambda Link style using built in Dot Net set operations for the collections.
             SetOperatorFunc =  (aDict, bDict, k) => bDict.Except(aDict, k).ToDictionary(ld => ld.Key, ld => ld.Value);
+        }
+    }
+    public class UnionAB : SetOperator
+    {
+        public UnionAB(DataChunk b, DataChunk a, DictCompareOnKeyOnly keyOnly, Options options)
+            : base(b, a, keyOnly, options)
+        {
+            // Here comes the magic a simple Union and force it back to Dictionary
+            // all in Lambda Link style using built in Dot Net set operations for the collections.
+            SetOperatorFunc = (aDict, bDict, k) => bDict.Union(aDict, k).ToDictionary(ld => ld.Key, ld => ld.Value);
         }
     }
 }

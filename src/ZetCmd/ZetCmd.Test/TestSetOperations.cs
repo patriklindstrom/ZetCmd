1dc5b9bd0f559c6007121ec97edefed405b9db6b
Patrik Lindström <patrik.lindstrom@lcube.se>
2015-05-26 11:30:16 +0200

    Added two more set test
    Test different DiffB test for different small sets.

--- src/ZetCmd/ZetCmd.Test/TestSetOperations.cs
+++ src/ZetCmd/ZetCmd.Test/TestSetOperations.cs
@@ -65,102 +65,104 @@ namespace ZetCmd.Test
              sutLineDict.Except(expectedDict).Count());
         }
 
-        //[Test]
-        //static public void ShouldGenerateSetDifferenceBetweenAand2()
-        //{
-        //    //Arrange
-        //    var opt = new Options
-        //    {
-        //        FileA = Mv.FileA
-        //        ,
-        //        FileB = Mv.FileB
-        //        ,
-        //        DiffB = Mv.DiffB
-        //        ,
-        //        IntersectAandB = Mv.IntersectAandB
-        //        ,
-        //        Verbose = Mv.Verbose
-        //        ,
-        //        Fieldseparator = Mv.Splitchar
-        //        ,
-        //        Keycolumns = Mv.ColKeys
-        //    };
-        //    var sut = new SetOperator();
-        //    var keyOnly = new DictCompareOnKeyOnly();
-        //    Dictionary<string, string> mockSetA = new Dictionary<string, string>
-        //    {
-        //        {"fum;fan;foo", Mv.Line},
-        //        //{"fum;fan;fee", Mv.Line2},
-        //        {"456;fan;faa", Mv.Line3}
-        //    };
-        //    Dictionary<string, string> mockSetB = new Dictionary<string, string>
-        //    {
-        //        {"fum;fan;foo", Mv.Line},
-        //        {"fum;fan;fee", Mv.Line2},
-        //        {"456;fan;faa", Mv.Line3},
-        //        {"NewFi;foo;fum", Mv.Line4}
-        //    };
-        //    var expectedDict = new Dictionary<string, string> { { "NewFi;foo;fum", Mv.Line4 } ,{"fum;fan;fee", Mv.Line2} };
-        //    var mockChunckA = new DataChunk(dataPath: Mv.DataPath, name: Mv.Name, option: opt)
-        //    {
-        //        LineDictionary = mockSetA
-        //    };
-        //    var mockChunkB = new DataChunk(dataPath: Mv.DataPath, name: Mv.Name, option: opt)
-        //    {
-        //        LineDictionary = mockSetB
-        //    };
-        //    //Act
-        //    var sutLineDict = sut.DiffB(a: mockChunckA, b: mockChunkB, keyOnly: keyOnly, options: opt);
-        //    //Assert
-        //    Assert.AreEqual(expectedDict.Except(sutLineDict).Count(),
-        //      sutLineDict.Except(expectedDict).Count());
-        //}
-        //[Test]
-        //static public void ShouldGenerateSetDifferenceBetweenAandB3()
-        //{
-        //    //Arrange
-        //    var opt = new Options
-        //    {
-        //        FileA = Mv.FileA
-        //        ,
-        //        FileB = Mv.FileB
-        //        ,
-        //        DiffB = Mv.DiffB
-        //        ,
-        //        IntersectAandB = Mv.IntersectAandB
-        //        ,
-        //        Verbose = Mv.Verbose
-        //        ,
-        //        Fieldseparator = Mv.Splitchar
-        //        ,
-        //        Keycolumns = Mv.ColKeys
-        //    };
-        //    var sut = new SetOperator();
-        //    var keyOnly = new DictCompareOnKeyOnly();
-        //    Dictionary<string, string> mockSetA = new Dictionary<string, string>
-        //    {
-        //        {"fum;fan;foo", Mv.Line},
-        //        //{"fum;fan;fee", Mv.Line2},
-        //        {"456;fan;faa", Mv.Line3}
-        //    };
-        //    Dictionary<string, string> mockSetB = new Dictionary<string, string>
-        //    {
-        //       // {"fum;fan;foo", Mv.Line},
-        //        {"fum;fan;fee", Mv.Line2},
-        //        {"456;fan;faa", Mv.Line3},
-        //        {"NewFi;foo;fum", Mv.Line4}
-        //    };
-        //    var expectedDict = new Dictionary<string, string> { { "NewFi;foo;fum", Mv.Line4 }, { "fum;fan;fee", Mv.Line2 } };
-        //    var mockChunckA = new DataChunk(dataPath: Mv.DataPath, name: Mv.Name, option: opt);
-        //    mockChunckA.LineDictionary = mockSetA;
-        //    var mockChunkB = new DataChunk(dataPath: Mv.DataPath, name: Mv.Name, option: opt);
-        //    mockChunkB.LineDictionary = mockSetB;
-        //    //Act
-        //    var sutLineDict = sut.DiffB(a: mockChunckA, b: mockChunkB, keyOnly: keyOnly, options: opt);
-        //    //Assert
-        //    Assert.AreEqual(expectedDict.Except(sutLineDict).Count(),
-        //      sutLineDict.Except(expectedDict).Count());
-        //}
+        [Test]
+        static public void ShouldGenerateSetDifferenceBetweenAand2()
+        {
+            //Arrange
+            var opt = new Options
+            {
+                FileA = Mv.FileA
+                ,
+                FileB = Mv.FileB
+                ,
+                DiffB = Mv.DiffB
+                ,
+                IntersectAandB = Mv.IntersectAandB
+                ,
+                Verbose = Mv.Verbose
+                ,
+                Fieldseparator = Mv.Splitchar
+                ,
+                Keycolumns = Mv.ColKeys
+            };
+           
+            var keyOnly = new DictCompareOnKeyOnly();
+            Dictionary<string, string> mockSetA = new Dictionary<string, string>
+            {
+                {"fum;fan;foo", Mv.Line},
+                //{"fum;fan;fee", Mv.Line2},
+                {"456;fan;faa", Mv.Line3}
+            };
+            Dictionary<string, string> mockSetB = new Dictionary<string, string>
+            {
+                {"fum;fan;foo", Mv.Line},
+                {"fum;fan;fee", Mv.Line2},
+                {"456;fan;faa", Mv.Line3},
+                {"NewFi;foo;fum", Mv.Line4}
+            };
+            var expectedDict = new Dictionary<string, string> { { "NewFi;foo;fum", Mv.Line4 }, { "fum;fan;fee", Mv.Line2 } };
+            var mockChunckA = new DataChunk(dataPath: Mv.DataPath, name: Mv.Name, option: opt)
+            {
+                LineDictionary = mockSetA
+            };
+            var mockChunkB = new DataChunk(dataPath: Mv.DataPath, name: Mv.Name, option: opt)
+            {
+                LineDictionary = mockSetB
+            };
+            var sut = new DiffB(a: mockChunckA, b: mockChunkB, keyOnly: keyOnly, options: opt);
+            //Act
+            var sutLineDict = sut.ReturnDictionary;
+            //Assert
+            Assert.AreEqual(expectedDict.Except(sutLineDict).Count(),
+              sutLineDict.Except(expectedDict).Count());
+        }
+        [Test]
+        static public void ShouldGenerateSetDifferenceBetweenAandB3()
+        {
+            //Arrange
+            var opt = new Options
+            {
+                FileA = Mv.FileA
+                ,
+                FileB = Mv.FileB
+                ,
+                DiffB = Mv.DiffB
+                ,
+                IntersectAandB = Mv.IntersectAandB
+                ,
+                Verbose = Mv.Verbose
+                ,
+                Fieldseparator = Mv.Splitchar
+                ,
+                Keycolumns = Mv.ColKeys
+            };
+            
+            var keyOnly = new DictCompareOnKeyOnly();
+            Dictionary<string, string> mockSetA = new Dictionary<string, string>
+            {
+                {"fum;fan;foo", Mv.Line},
+                //{"fum;fan;fee", Mv.Line2},
+                {"456;fan;faa", Mv.Line3}
+            };
+            Dictionary<string, string> mockSetB = new Dictionary<string, string>
+            {
+               // {"fum;fan;foo", Mv.Line},
+                {"fum;fan;fee", Mv.Line2},
+                {"456;fan;faa", Mv.Line3},
+                {"NewFi;foo;fum", Mv.Line4}
+            };
+            var expectedDict = new Dictionary<string, string> { { "NewFi;foo;fum", Mv.Line4 }, { "fum;fan;fee", Mv.Line2 } };
+            var mockChunckA = new DataChunk(dataPath: Mv.DataPath, name: Mv.Name, option: opt);
+            mockChunckA.LineDictionary = mockSetA;
+            var mockChunkB = new DataChunk(dataPath: Mv.DataPath, name: Mv.Name, option: opt);
+            mockChunkB.LineDictionary = mockSetB;
+            var sut = new DiffB(a: mockChunckA, b: mockChunkB, keyOnly: keyOnly, options: opt);
+            //Act
+            var sutLineDict = sut.ReturnDictionary;
+            //Assert
+            Assert.AreEqual(expectedDict.Except(sutLineDict).Count(),
+              sutLineDict.Except(expectedDict).Count());
+        }
     }
 }
 

5b657081d5536744f15ca2197b6b61b8f392cd44
Patrik Lindstrom <patriklindstrom@hotmail.com>
2015-06-06 17:26:54 +0200

    Change output path for build script
    I renamed from OutputBuild to Build so Git would ignore it.

--- src/ZetCmd/Build.ps1
+++ src/ZetCmd/Build.ps1
@@ -1,10 +1,10 @@
 Copy-Item ".\ZetCmd\bin\Debug\ZetCmd.exe" -Destination ".\OutPutBuild"
 Copy-Item ".\ZetCmd\bin\Debug\CommandLine.dll" -Destination ".\OutPutBuild"
 # http://www.generatedata.com/
-Copy-Item ".\TestFiles\A_TestFile.csv" -Destination ".\OutPutBuild"
-Copy-Item ".\TestFiles\B_TestFile.csv" -Destination ".\OutPutBuild"
+Copy-Item ".\TestFiles\A_TestFile.csv" -Destination ".\Build"
+Copy-Item ".\TestFiles\B_TestFile.csv" -Destination ".\Build"
 Push-Location
-cd ".\OutPutBuild"
+cd ".\Build"
 ls
 Write-host	'-v -aA_TextFile.csv  -bB_TestFile.csv -k1 2 3 -s;'
 cmd.exe /c start cmd 
